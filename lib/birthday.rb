3ebd6b2177f32bfc73c680b0dce3cbcbc4618cf6
Tara Ellicott <taraellicott@gmail.com>
2018-04-14 16:30:36 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,7 @@ birthday_kids = {
 }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each 
-  do |kids_name, age|
+  birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
